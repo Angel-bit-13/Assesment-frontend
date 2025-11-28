ecd6ec7d7aceede00aa01bffc448ea0a954df08a
Angelin <angelinraison@gmail.com>
2025-11-28 21:20:32 +0530

    Add font-serif class to navigation links

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,11 @@ const App = () => {
         <div className='max-w-7x1 mx-auto px-4 sm:px-6 lg:px-8'>
           <ul className='flex space-x-6 py-4'>
             <li>
-              <Link to="/" className='text-white hover:text-gray-300'>Home</Link>
+              <Link to="/" className='text-white hover:text-gray-300 font-serif'>Home</Link>
             </li>
             
             <li>
-              <Link to="/addproduct" className='text-white hover:text-gray-300'>Add Product</Link>
+              <Link to="/addproduct" className='text-white hover:text-gray-300 font-serif'>Add Product</Link>
             </li>
           </ul>
         </div>
