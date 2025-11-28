c7339a23d645a568c6b87133d25847ad87a04db7
Angelin <angelinraison@gmail.com>
2025-11-28 20:09:22 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+        setLoading(false);
+      })
+      .catch(() => {
+        setError('Failed to load products.');
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <h2 style={{ textAlign: 'center' }}>Loading...</h2>;
+  if (error) return <h2 style={{ textAlign: 'center', color: 'red' }}>{error}</h2>;
+
   return (
-    <div className="flex flex-col min-h-screen items-center justify-center bg-gradient-to-b from-cyan-100 to-cyan-300 p-8">
-        <div className="min-h-screen bg-[url('img.jpeg')] bg-cover">
+    <div className='py-10 px-5 bg-cyan-100 min-h-screen'>
+      <h1 className='font-semibold text-center font-serif text-2xl caret-cyan-900'>PRODUCTS</h1>
 
-      <div className="text-center max-w-2xl">
-        <h1 className="text-5xl font-extrabold text-cyan-800 mb-5 mt-50">
-          Welcome to Home
-        </h1>
+      <div className='grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-5 mt-6'>
+        {products.map((product) => (
+          <div
+            key={product.id}
+            className="border border-gray-300 rounded-lg p-4 bg-white shadow-md"
+          >
+            <img
+            src={product.image}
+            alt={product.title}
+            className="w-full h-[180px] object-contain"
+          />
 
-        <p className="text-gray-600 text-lg mb-8">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.  assumenda nulla dolorem tempore perferendis! Et veritatis aliquam quisquam?.
-        </p>
-
-        <button className="bg-cyan-700 hover:bg-cyan-900 text-white py-3 px-8 rounded-lg shadow-lg transition">
-          Get Started
-        </button>
-        
+          <h3 className="text-[16px] mt-2.5">
+              {product.title.length > 50
+                ? product.title.substring(0, 50) + '...'
+                : product.title}
+            </h3>
+            <p><strong>Category:</strong> {product.category}</p>
+            <p><strong>Price:</strong> ${product.price}</p>
+          </div>
+        ))}
       </div>
-      
-</div>
-      </div>
-    
+    </div>
   );
 };
 
